7a8c1f2fd9415b41a1738707689d0b9bbc021e3b
Neel Bommisetty <neel250294@gmail.com>
2024-11-01 21:26:22 -0700

    Sitemap and Robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/rate/", "/api/"],
+      },
+    ],
+    sitemap: "https://www.dragoncityhelper.com/sitemap.xml",
+  };
+}
---
+++ src/app/sitemap.ts
@@ -0,0 +1,23 @@
+import { fetchAllDragonIds } from "@/services/dragons";
+import type { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = "https://www.dragoncityhelper.com";
+
+  // Fetch dynamic routes, e.g., dragon IDs
+  const dragonIds = await fetchAllDragonIds();
+
+  // Define static routes
+  const staticRoutes = ["", "/tierlist", "/terms", "/privacy"].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  // Define dynamic routes
+  const dynamicRoutes = dragonIds.map((id) => ({
+    url: `${baseUrl}/dragon/${id}`,
+    lastModified: new Date().toISOString(),
+  }));
+
+  return [...staticRoutes, ...dynamicRoutes];
+}
